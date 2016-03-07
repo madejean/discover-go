f4a50dd2d256c07872ad0471cfb553a32f9d4595
Marine Dejean <marine@Admins-MacBook-Air.local>
2016-03-06 20:32:31 -0800

    finished task 2

--- 2-struct/main.go
+++ 2-struct/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	type user struct {
-		Name string `json: "name"`
-		DOB  string `json: "date_of_birth, string"`
-		City string `json: "city"`
+		Name string `json:"name"`
+		DOB  string `json:"date_of_birth"`
+		City string `json:"city"`
 	}
-	fmt.Println("Hello")
+	u := user{Name: "Betty Holberton", DOB: "March 7, 1917", City: "Philadelphia"}
+	fmt.Println("Hello", u.Name)
+	t, err := time.Parse("January 2, 2006", u.DOB)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s who was born in %s would be %d years old today.\n", u.Name, u.City, time.Now().Year()-t.Year())
+	}
 }
