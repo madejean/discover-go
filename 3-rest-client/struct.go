df3d8b4ad0f108240c3a05bca73ba6d664d06be0
Marine Dejean <marine@Admins-MacBook-Air.local>
2016-03-06 20:53:35 -0800

    created files and struct

---
+++ 3-rest-client/struct.go
@@ -0,0 +1,24 @@
+package main
+
+type movie struct {
+	Title      string `json:"Title"`
+	Year       string `json:"Year"`
+	Rated      string `json:"Rated"`
+	Released   string `json:"Released"`
+	Runtime    string `json:"Runtime"`
+	Genre      string `json:"Genre"`
+	Director   string `json:"Director"`
+	Writer     string `json:"Writer"`
+	Actors     string `json:"Actors"`
+	Plot       string `json:"Plot"`
+	Language   string `json:"Language"`
+	Country    string `json:"Country"`
+	Awards     string `json:"Awards"`
+	Poster     string `json:"Poster"`
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+	ImdbID     string `json:"imdbID"`
+	Type       string `json:"Type"`
+	Response   string `json:"Response"`
+}
