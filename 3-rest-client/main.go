75bc76e5e51b20667ed2a9f22e6b283ea9e85794
madejean <madejean007@hotmail.fr>
2016-03-07 15:07:18 -0800

    finished task 3

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -4,9 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+
+	type movie struct {
+		Title      string `json:"Title"`
+		Year       string `json:"Year"`
+		Rated      string `json:"Rated"`
+		Released   string `json:"Released"`
+		Runtime    string `json:"Runtime"`
+		Genre      string `json:"Genre"`
+		Director   string `json:"Director"`
+		Writer     string `json:"Writer"`
+		Actors     string `json:"Actors"`
+		Plot       string `json:"Plot"`
+		Language   string `json:"Language"`
+		Country    string `json:"Country"`
+		Awards     string `json:"Awards"`
+		Poster     string `json:"Poster"`
+		Metascore  string `json:"Metascore"`
+		ImdbRating string `json:"imdbRating"`
+		ImdbVotes  string `json:"imdbVotes"`
+		ImdbID     string `json:"imdbID"`
+		Type       string `json:"Type"`
+		Response   string `json:"Response"`
+	}
+
 	resp, err := http.Get("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
 	if err != nil {
 		fmt.Println(err)
@@ -19,5 +44,7 @@ func main() {
 		fmt.Println("err", err)
 		return
 	}
-	fmt.Printf("The movie : %s was released in %s - the IMBD rating is %d%% with %s votes\n", u.Title, u.Year, u.ImdbRating, u.ImdbVotes)
+	p, err := strconv.ParseFloat(u.ImdbRating, 32)
+	p = (p / 10) * 100
+	fmt.Printf("The movie : %s was released in %s - the IMBD rating is %d%% with %s votes\n", u.Title, u.Year, int(p), u.ImdbVotes)
 }
